8209466e6f5da1536535e0ae3da7d660d505e705
Fiip M <filipmitrovic@hotmail.com>
2021-04-08 12:29:23 +0200

    updates

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const mobileNav = document.querySelector('.mobile-nav'); 
+const pageWrapper = document.querySelector('.page-wrapper');
+const icons = document.querySelectorAll('.icon');
+
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('hidden');
+})
+
+hamburgerIcon.addEventListener('click', () => {
+    hamburgerIcon.classList.toggle('change');
+    mobileNav.classList.toggle('show');
+    document.body.classList.toggle('overflow');
+    pageWrapper.classList.toggle('lock-screen');
+})
+
+// add current year in footer
+document.querySelector('.copyright-year').textContent = new Date().getFullYear();
+
+for (const icon of icons) {
+    icon.addEventListener('click', () => {
+        icon.classList.toggle('show-tooltip');
+    })
+}
